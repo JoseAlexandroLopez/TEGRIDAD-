2a1afbec090be4a75232a67584afea69731b53ce
Jose Alexandro Lopez <josealexandrolopez2004@gmail.com>
2026-06-23 19:44:21 -0500

    fix: desactivando conexion a BD para evitar caida de render

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import app from './src/services/server.js'; // SIn llaves, porque es export default
 import dotenv from 'dotenv';
-import './src/config/db.js'; // Despierta la conexión a MySQL
+// import './src/config/db.js'; // 🛑 DESACTIVADO PARA EL MODO DEMO (Evita que Render se rompa)
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor unificado corriendo en http://localhost:${PORT}`);
+  console.log(`🚀 Servidor unificado corriendo en el puerto ${PORT} (Modo Demo sin DB)`);
 });
